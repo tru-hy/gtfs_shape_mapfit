3067994a45c5c030584d42af8c1a657d476b839b
Jami Pekkanen <jami.pekkanen@helsinki.fi>
2014-08-01 15:40:12 +0300

    Added a script to fit all the stuff in one go

---
+++ fit_gtfs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+MAP_FILE=$1
+PROJECTION=$2
+GTFS_FILE=$3
+RESULT_FILE=$4
+
+if [ $# -lt 4 ]
+then
+	echo "Usage: $0 map_file projection original_gtfs_file new_gtfs_file"
+	exit 1
+fi
+
+TMP_DIR=`mktemp -d -t fit_gtfs.XXXXX`
+GTFS_DIR=$TMP_DIR/orig_gtfs
+mkdir $GTFS_DIR
+unzip $GTFS_FILE -d $TMP_DIR/orig_gtfs
+
+./gtfs_stop_cleaner.py $MAP_FILE $GTFS_DIR/stops.txt > $TMP_DIR/stops.fitted.txt
+./gtfs_shape_mapfit2.py $MAP_FILE $PROJECTION $GTFS_DIR \
+	2>&1 >$TMP_DIR/shapes.fitted.txt |tee $TMP_DIR/shapefit_stats.txt >&2
+./filter_bad_fits.py $TMP_DIR/shapefit_stats.txt $TMP_DIR/shapes.fitted.txt $GTFS_DIR/shapes.txt \
+	> $TMP_DIR/shapes.filtered.txt
+
+cp $TMP_DIR/shapes.filtered.txt $GTFS_DIR/shapes.txt
+cp $TMP_DIR/stops.fitted.txt $GTFS_DIR/stops.txt
+
+zip $RESULT_FILE $GTFS_DIR/*
+
+rm -r $TMP_DIR
